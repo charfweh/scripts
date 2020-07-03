e40d5ac66a4e9f02bb45e80d917a9b8872003295
charfweh <affan.ismail2016@gmail.com>
2020-07-03 16:52:55 +0530

    uh

--- the_damn_vote.js
+++ the_damn_vote.js
@@ -3,7 +3,7 @@ const cron = require('node-cron')
 const ion = "https://top.gg/bot/449719890312691715/vote"
 
 // reference cron help : minute hours day-of-month month day-of-week
-cron.schedule("5 11,23 * * *",()=>{
+cron.schedule("5,15,30 11,23,16 * * *",()=>{
     console.log("started the cron job")
     open(ion).then(console.log("Its time to vote"))
 })
