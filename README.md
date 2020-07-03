e863c63d1a14b5f79d613cc1b0ca52686f4c07dc
charfweh <affan.ismail2016@gmail.com>
2020-07-03 16:33:09 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# nodejs_scripts
+# the damn vote
+starting web browser to vote the damn bot because uh well lets just relax
---
+++ the_damn_vote.js
@@ -0,0 +1,9 @@
+const open = require('open')
+const cron = require('node-cron')
+const ion = "https://top.gg/bot/449719890312691715/vote"
+
+// reference cron help : minute hours day-of-month month day-of-week
+cron.schedule("5 11,22 * * *",()=>{
+    console.log("started the cron job")
+    open(ion).then(console.log("Its time to vote"))
+})
